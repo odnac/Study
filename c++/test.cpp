f36dbd450818ed73b219c0c051eb2b70130378b3
odnac <odnac777@gmail.com>
2026-03-17 18:04:46 +0900

    test.cpp

---
+++ c++/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // 콘솔창에 문장을 출력합니다.
+    std::cout << "Hello, C++ World!" << std::endl;
+    std::cout << "윈도우와 맥북에서 모두 잘 작동합니다." << std::endl;
+
+    return 0;
+}
