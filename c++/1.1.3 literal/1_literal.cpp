2d096042e9c38997270d35faf6b949537d7eabb2
odnac <odnac777@gmail.com>
2026-03-18 17:40:02 +0900

    literal

---
+++ c++/1.1.3 literal/1_literal.cpp
@@ -0,0 +1,19 @@
+
+// Literal: A value such as a number or a string.
+
+/*
+    Integer Literals:
+    Decimal (Base 10)     : 123
+    Octal (Base 8)        : 0173
+    Hexadecimal (Base 16) : 0x7B
+    Binary (Base 2)       : 0b1111011
+*/
+
+/*
+    Floating-Point  Literals          :
+    Floating-point  (float)           : 3.14f
+    Double          precision (double): 3.14
+    Hexadecimal     floating-point    : 0x3.ABCp-10
+    Single          character         : 'a'
+    Null-terminated character array   : "character array"
+*/
