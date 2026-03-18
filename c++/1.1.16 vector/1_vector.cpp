5f9cf544e465d95428c360cbda5c2c9aeea652f9
odnac <odnac777@gmail.com>
2026-03-18 22:50:12 +0900

    vector

--- c++/1.1.16 vector/1_vector.cpp
+++ c++/1.1.16 vector/1_vector.cpp
@@ -8,8 +8,8 @@ int main()
 {
     vector<int> myVec{11, 22};
 
-    myVec.push_back(33);
-    myVec.push_back(44);
+    myVec.push_back(33); // push_back
+    myVec.push_back(44); // push_back
 
     cout << format("1st element = {}", myVec[0]) << endl;
     cout << format("2nd element = {}", myVec[1]) << endl;
