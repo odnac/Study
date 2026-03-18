18b4ff7cbaf347168fb8c8649551e6029305dbb4
odnac <odnac777@gmail.com>
2026-03-18 21:25:27 +0900

    conditional statement

---
+++ c++/1.1.8 conditional statement/1_if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /*
+        if( Initializer; condition ) // Initializer is scoped only to this if-statement
+        {
+            ...
+        }
+        else if()
+        {
+            ...
+        }
+        else
+        {
+            ...
+        }
+    */
+
+    return 0;
+}
---
+++ c++/1.1.8 conditional statement/2_switch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /*
+        switch()
+        {
+            case a:
+                ...
+                break;
+            case b:
+                ...
+                break;
+            default:
+                ...
+                break;
+        }
+    */
+
+    /*
+        switch()
+        {
+            case a:
+                ...
+                [[fallthrough]]
+            case b:
+                ...
+                break;
+            default:
+                ...
+                break;
+        }
+    */
+
+    return 0;
+}
