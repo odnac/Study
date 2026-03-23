d5ae4704b6e1ee9bc0fb6c8f0744e962a4ed6ae8
odnac <odnac777@gmail.com>
2026-03-23 19:31:27 +0900

    type alias

---
+++ c++/1.1.33 type alias/type_alias.cpp
@@ -0,0 +1,48 @@
+
+/*
+    using IntPtr = int* // type alias
+
+    int* p1;
+    intPtr p2;
+
+    p1 = p2;
+    p2 = p1;
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#if 0
+void processVector(const std::vector<std::basic_string<char>> &vec) //
+{
+    /* ... */
+}
+
+int main()
+{
+ 
+    std::vector<std::basic_string<char>> myVector; //
+    processVector(myVector);
+
+return 0;
+}
+
+#else
+
+using string = std::basic_string<char>; // type alias
+
+void processVector(const std::vector<string> &vec) //
+{
+    /* ... */
+}
+
+int main()
+{
+
+    std::vector<string> myVector; //
+    processVector(myVector);
+
+    return 0;
+}
+#endif
