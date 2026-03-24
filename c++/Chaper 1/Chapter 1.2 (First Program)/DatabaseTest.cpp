7b57cafd29895fc89b3587744c0b422884185856
odnac <odnac777@gmail.com>
2026-03-24 13:42:33 +0900

    Employee Program

---
+++ c++/Chaper 1/Chapter 1.2 (First Program)/DatabaseTest.cpp
@@ -0,0 +1,39 @@
+// DatabaseTest.cpp
+
+/*
+    import <iostream>;
+    import database;
+    import employee;
+
+    using namespace std;
+    using namespace Records;
+
+    int main()
+    {
+        Database myDB;
+
+        Employee& emp1{ myDB.addEmployee("Greg", "Wallis") };
+        emp1.fire();
+
+        Employee& emp2{ myDB.addEmployee("Marc", "White") };
+        emp2.setSalary(100000);
+
+        Employee& emp3{ myDB.addEmployee("John", "Doe") };
+        emp3.setSalary(100000);
+        emp3.promote();
+
+        cout << "all employees: " << endl
+            << endl;
+        myDB.displayAll();
+
+        cout << endl
+            << "current employees: " << endl
+            << endl;
+        myDB.displayCurrent();
+
+        cout << endl
+            << "former employees: " << endl
+            << endl;
+        myDB.displayFormer();
+    }
+*/
---
+++ c++/Chaper 1/Chapter 1.2 (First Program)/EmployeeTest.cpp
@@ -0,0 +1,23 @@
+// EmployeeTest.cpp
+
+/*
+    import <iostream>;
+    import employee;
+
+    using namespace std;
+    using namespace Records;
+
+    int main()
+    {
+        cout << "Testing the Employee class." << endl;
+        Employee emp{"Jane", "Doe"};
+        emp.setFirstName("Jone");
+        emp.setLastName("Doe");
+        emp.setEmployeeNumber(71);
+        emp.setSalary(50000);
+        emp.promote();
+        emp.promote(50);
+        emp.hire();
+        emp.display();
+    }
+*/
